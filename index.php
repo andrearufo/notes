f5fe23693de5ced4e1725f39ea7d81847b2a0df2
Andrea Rufo <info@andrearufo.it>
2019-10-16 12:12:49 +0200

    Add debugger

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+// Debugger
+$debug = true;
+if( $debug ) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
 // Classes
 require 'core/Note.php';
 require 'core/Parsedown.php';
